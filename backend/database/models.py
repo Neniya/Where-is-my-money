ed49525810def857ee216acbbd58949607724872
Evgeniya Kotova <zheniyasl@gmail.com>
2021-06-02 18:38:10 +0200

    correct database relationships

--- backend/database/models.py
+++ backend/database/models.py
@@ -6,7 +6,8 @@ db = SQLAlchemy()
 
 database_name = "mymoney"
 DB_USERNAME = "postgres"
-DB_PASSWORD = ""
+DB_PASSWORD = "postgres123"
+#print(os.environ['DATABASE_URL'])
 database_path = os.environ['DATABASE_URL'] \
     if 'DATABASE_URL' in os.environ \
     else "postgresql://{}:{}@{}/{}".format(
@@ -21,7 +22,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+   # db.create_all()
 
 '''
 db_drop_and_create_all()
@@ -41,28 +42,34 @@ class Account(db.Model):
     __tablename__ = 'accounts'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(), nullable=False)
+    circulations = db.relationship('Monetary_circulation', backref='account', lazy = True)
+
 
 class Cost_type(db.Model):
     __tablename__ = 'cost_types'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(), nullable=False)
+    items = db.relationship('cost_items', backref='type', lazy = True)
 
 class Cost_item(db.Model):
     __tablename__ = 'cost_items'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable = False)
     spending = db.Column(db.Boolean, nullable = False, default = False)
-    type_id = db.ForeignKey('cost_types.id', nullable = False)
+    type_id = db.Column(db.Integer, db.ForeignKey('cost_types.id'), nullable = False)
+    circulations = db.relationship('Monetary_circulation', backref='cost_item', lazy = True)
 
 class Currency(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable = False)
+    circulations = db.relationship('Monetary_circulation', backref='currency', lazy = True)
+
 
 class  Monetary_circulation(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     cost_item_id = db.ForeignKey('cost_items.id', nullable = False)
     notice = db.Column(db.String, nullable = True)
-    income = db.Column(db.Float(precision='20,2'), server_default = 0.00)
-    spending = db.Column(db.Float(precision='20,2'), server_default = 0.00)
-    currency_id = db.ForeignKey('currency.id', nullable = False)
-    account_id = db.ForeignKey('account.id', nullable = False)
+    income = db.Column(db.Numeric(10,2))
+    spending = db.Column(db.Numeric(10,2))
+    currency_id = db.Column(db.ForeignKey('currency.id'), nullable = False)
+    account_id = db.Column(db.ForeignKey('accounts.id'), nullable = False)
--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,3 +1,32 @@
 import os
+import sys
+sys.path.append('database')
 from flask import Flask
 
+from flask_cors import CORS
+from flask_migrate import Migrate
+from models import (
+    setup_db,
+    db
+)
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    migrate = Migrate(app, db)
+
+    @app.after_request
+    def after_reguest(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type, Autrorizaition')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
+
+    return app
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
