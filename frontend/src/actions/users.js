20735b32d9d7ae8642f6347bb645d7d69d812668
Evgeniya Kotova <zheniyasl@gmail.com>
2021-11-23 14:32:21 +0100

    correct redux action for get user

--- frontend/src/actions/users.js
+++ frontend/src/actions/users.js
@@ -1,6 +1,8 @@
-export const GET_USERS = "GET_USERS";
+export const GET_USERS = 'GET_USERS';
 
-export const getUsers = (users) =>{
+export const getUsers = (users) => {
+  return {
     type: GET_USERS,
     users,
-}
+  };
+};
