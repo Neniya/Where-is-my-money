d3e8d14da3e10aa143879b72d38a9abef20d538d
Evgeniya Kotova <zheniyasl@gmail.com>
2021-12-23 15:39:15 +0100

    change styles for newTransaction

--- frontend/src/components/NewTransaction.js
+++ frontend/src/components/NewTransaction.js
@@ -1,30 +1,56 @@
 import React, { useState } from 'react';
+import './NewTransaction.css';
 
 const NewTransaction = (props) => {
   const [sum, setSum] = useState(0.01);
   return (
-    <div className="tableLine">
-      <input className="data_cell" type="date" />
-      <select className="data_cell">
-        <option>gym</option>
-      </select>
-      <input
-        className="sum_cell data_cell"
-        type="number"
-        step="0.01"
-        value={sum}
-        onChange={(e) => {
-          setSum(e.target.value);
-        }}
-      ></input>
-      <select className="data_cell">
-        <option>euro</option>
-      </select>
-      <input className="data_cell" type="text"></input>
-      <select className="data_cell">
-        <option>account</option>
-      </select>
-    </div>
+    <form className="new_transaction_form el_radius">
+      <div className="main_data_new_transaction">
+        <div className="left_data vertical_data">
+          <div>
+            <label className="lbl">Date:</label>
+            <input className="new_cell el_radius " type="date" />
+          </div>
+          <div>
+            <span>
+              <label className="lbl">Title:</label>
+              <select className="new_cell el_radius">
+                <option>gym</option>
+              </select>
+            </span>
+          </div>
+        </div>
+
+        <div className="right_data vertical_data">
+          <div>
+            <label className="lbl">From:</label>
+            <select className="new_cell el_radius">
+              <option>account</option>
+            </select>
+          </div>
+          <div>
+            <label className="lbl">Sum:</label>
+            <input
+              className="sum_cell new_cell el_radius"
+              type="number"
+              step="0.01"
+              value={sum}
+              onChange={(e) => {
+                setSum(e.target.value);
+              }}
+            ></input>
+            <select className="new_cell el_radius ">
+              <option>euro</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <lable className="lbl">Notes:</lable>
+        <input type="text" className="new_cell el_radius new_notes"></input>
+      </div>
+    </form>
   );
 };
 
