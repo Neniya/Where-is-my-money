9579f7cfb4d27b41a03ddd0e883324d94f00394e
Evgeniya Kotova <zheniyasl@gmail.com>
2022-03-28 17:00:50 +0200

    add post data in api frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -92,3 +92,21 @@ export const getUserAccounts = async (id) => {
     console.log('error', error);
   }
 };
+
+export const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
