63ef364c7d2b4a59e2b55be790827d8c7ffb0c36
Evgeniya Kotova <zheniyasl@gmail.com>
2022-05-03 20:26:39 +0200

    add change circulation to redux actions and reducers

--- frontend/src/actions/userCirculations.js
+++ frontend/src/actions/userCirculations.js
@@ -1,9 +1,10 @@
-import { postData, deleteCirculation } from '../utils/api';
+import { postData, deleteCirculation, changeCirculation } from '../utils/api';
 import { showLoading, hideLoading } from 'react-redux-loading';
 
 export const GET_USER_CIRCULATIONS = 'GET_USER_CIRCULATIONS';
 export const ADD_USER_CIRCULATION = 'ADD_USER_CIRCULATION';
 export const DELETE_USER_CIRCULATION = 'DELETE_USER_CIRCULATION';
+export const CHANGE_USER_CIRCULATION = 'CHANGE_USER_CIRCULATION';
 
 export const getCirculationsForUser = (circulations) => {
   return {
@@ -15,6 +16,13 @@ export const getCirculationsForUser = (circulations) => {
 export const addUserCirculation = (circulation) => {
   return {
     type: ADD_USER_CIRCULATION,
+    circulation,
+  };
+};
+
+export const changeUserCirculation = (circulation) => {
+  return {
+    type: CHANGE_USER_CIRCULATION,
     circulation,
   };
 };
@@ -41,6 +49,28 @@ export function handleAddUserCirculation(circulation) {
         dispatch(
           addUserCirculation({
             id: data.created,
+            user_id: authedUser,
+            ...circulation,
+          })
+        );
+      })
+      .then(() => dispatch(hideLoading));
+  };
+}
+
+export function handleChangeUserCirculation(circulation) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    dispatch(showLoading);
+
+    return changeCirculation(circulation.id, {
+      user_id: authedUser,
+      ...circulation,
+    })
+      .then((data) => {
+        circulation.date = data.date;
+        dispatch(
+          changeUserCirculation({
             user_id: authedUser,
             ...circulation,
           })
--- frontend/src/reducers/userCirculatios.js
+++ frontend/src/reducers/userCirculatios.js
@@ -2,17 +2,21 @@ import {
   GET_USER_CIRCULATIONS,
   ADD_USER_CIRCULATION,
   DELETE_USER_CIRCULATION,
+  CHANGE_USER_CIRCULATION,
 } from '../actions/userCirculations';
 
 export default function userCirculations(state = [], action) {
   switch (action.type) {
     case GET_USER_CIRCULATIONS:
       return [...state, ...action.circulations];
-
     case ADD_USER_CIRCULATION:
       return [...state, action.circulation];
     case DELETE_USER_CIRCULATION:
       return state.filter((circulation) => circulation.id !== action.id);
+    case CHANGE_USER_CIRCULATION:
+      return state.map((circ) =>
+        circ.id === action.circulation.id ? action.circulation : circ
+      );
 
     default:
       return state;
